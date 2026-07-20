adb799abb1ede7288c20121056847e9294b0fc93
Valentin Chmara <valentinchmara@gmail.com>
2026-07-20 10:59:21 +0200

    chore: clean up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,29 @@
-import { defineOrganization } from 'nuxt-schema-org/schema'
+import { definePerson } from 'nuxt-schema-org/schema'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui', '@nuxt/content', '@vueuse/nuxt', '@nuxtjs/seo', 'motion-v/nuxt', 'nuxt-llms', '@nuxt/scripts', '@nuxt/fonts'],
+
+  $production: {
+    scripts: {
+      registry: {
+        databuddyAnalytics: {
+          clientId: '7ef09b14-bd15-4d93-85db-5eae3d19c870',
+          trackInteractions: true,
+          trackEngagement: true,
+          trackOutgoingLinks: true,
+          trackScrollDepth: true,
+          trackExitIntent: true,
+          trackBounceRate: true,
+          trackWebVitals: true,
+          enableBatching: true,
+          scriptOptions: {
+            trigger: 'onNuxtReady'
+          }
+        }
+      }
+    }
+  },
+
   devtools: {
     enabled: true
   },
@@ -50,25 +72,18 @@ export default defineNuxtConfig({
 
   schemaOrg: {
     reactive: true,
-    identity: defineOrganization({
-      name: 'Qleer.ai',
-      logo: 'https://app.qleer.ai/favicon.png'
+    identity: definePerson({
+      name: 'Valentin Chmara',
+      image: '/avatar.png',
+      description: 'I\'m a Software Engineer with a passion for building innovative solutions.',
+      url: 'https://valentinchmara.com',
+      jobTitle: 'Software Engineer',
+      worksFor: 'Qleer.ai',
+      sameAs: [
+        'https://www.linkedin.com/in/valentinchmara/',
+        'https://github.com/vachmara',
+        'https://x.com/vachmara'
+      ]
     })
-  },
-
-  scripts: {
-    registry: {
-      databuddyAnalytics: {
-        clientId: '7ef09b14-bd15-4d93-85db-5eae3d19c870',
-        trackInteractions: true,
-        trackEngagement: true,
-        trackOutgoingLinks: true,
-        trackScrollDepth: true,
-        trackExitIntent: true,
-        trackBounceRate: true,
-        trackWebVitals: true,
-        enableBatching: true
-      }
-    }
   }
 })
